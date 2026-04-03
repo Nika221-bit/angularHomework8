8a5474db0b768355713bd191ed52b25fd838f66e
Nikoloz_22 <K0K0L0z1922@gmail.com>
2026-04-04 01:56:19 +0400

    function done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Hotel } from './hotel/hotel';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:Home},
+
+    {path:'hotel',component:Hotel, 
+           
+        children:[
+            {path:'about',component:About},
+            {path:'contact',component:Contact}
+
+        ]
+
+    }
+];
---
+++ src/app/header/header.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+  showChildren = signal(false);
+
+  toggleChildren() {
+    this.showChildren.update(value => !value);
+  }
+}
